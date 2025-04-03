53612cd8736713b5623254193c0416e4aae98ed3
Mohd Shahabuddin <60789263+Shahab-Sidd@users.noreply.github.com>
2025-04-04 03:43:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/sacred-trails-nepal-yatra/",
+  base: mode === "production" ? "/sacred-trails-nepal-yatra/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
