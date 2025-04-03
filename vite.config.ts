ce368939b0e77a53f7ba4995691e32a2ad5434ca
Mohd Shahabuddin <60789263+Shahab-Sidd@users.noreply.github.com>
2025-04-04 03:34:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sacred-trails-nepal-yatra/",
   server: {
     host: "::",
     port: 8080,
